1c6bf2eb39a04b5a91daa6c97cb3d81a85f9a5cb
URB444 <87970004+URB444@users.noreply.github.com>
2025-08-05 09:36:24 +0530

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -2,36 +2,31 @@
 from time import sleep
 import sys
 
+def check_vitals(temperature, pulseRate, spo2):
+    """Pure function: returns (status, message) tuple."""
+    if temperature > 102 or temperature < 95:
+        return False, 'Temperature critical!'
+    elif pulseRate < 60 or pulseRate > 100:
+        return False, 'Pulse Rate is out of range!'
+    elif spo2 < 90:
+        return False, 'Oxygen Saturation out of range!'
+    return True, ''
+
+def blink_alert(times=6, delay=1):
+    """Handles the blinking alert animation."""
+    for _ in range(times):
+        print('\r* ', end='')
+        sys.stdout.flush()
+        sleep(delay)
+        print('\r *', end='')
+        sys.stdout.flush()
+        sleep(delay)
+    print()  # Move to next line after blinking
 
 def vitals_ok(temperature, pulseRate, spo2):
-  if temperature > 102 or temperature < 95:
-    print('Temperature critical!')
-    for i in range(6):
-      print('\r* ', end='')
-      sys.stdout.flush()
-      sleep(1)
-      print('\r *', end='')
-      sys.stdout.flush()
-      sleep(1)
-    return False
-  elif pulseRate < 60 or pulseRate > 100:
-    print('Pulse Rate is out of range!')
-    for i in range(6):
-      print('\r* ', end='')
-      sys.stdout.flush()
-      sleep(1)
-      print('\r *', end='')
-      sys.stdout.flush()
-      sleep(1)
-    return False
-  elif spo2 < 90:
-    print('Oxygen Saturation out of range!')
-    for i in range(6):
-      print('\r* ', end='')
-      sys.stdout.flush()
-      sleep(1)
-      print('\r *', end='')
-      sys.stdout.flush()
-      sleep(1)
-    return False
-  return True
+    status, message = check_vitals(temperature, pulseRate, spo2)
+    if not status:
+        print(message)
+        blink_alert()
+        return False
+    return True
