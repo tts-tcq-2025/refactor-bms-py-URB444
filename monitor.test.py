480832f3ae158cf3019e1bd52a0d45cdffe14c5c
URB444 <87970004+URB444@users.noreply.github.com>
2025-08-18 23:09:36 +0530

    Update monitor.test.py

--- monitor.test.py
+++ monitor.test.py
@@ -6,7 +6,7 @@ class MonitorTest(unittest.TestCase):
     def test_not_ok_when_any_vital_out_of_range(self):
         self.assertFalse(vitals_ok(94, 90, 70))
         self.assertFalse(vitals_ok(104, 90, 70))
-        self.assertFasle(vitals_ok(98.1, 70, 98))
+        self.assertFalse(vitals_ok(98.1, 70, 98))
         self.assertTrue(vitals_ok(95.2, 70, 50))
         self.assertTrue(vitals_ok(101, 70, 50))
         self.assertTrue(vitals_ok(99, 61, 50))
@@ -16,4 +16,5 @@ class MonitorTest(unittest.TestCase):
 
 if __name__ == '__main__':
   unittest.main()
+
 
